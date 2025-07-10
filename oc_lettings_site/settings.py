f3d3331b28ee9d04e261bf501f5ed1226224753a
Anthony YOU <spleen85@proton.me>
2025-07-10 10:53:00 +0200

    Resolve flake8 violations

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -111,4 +111,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static",]
+STATICFILES_DIRS = [BASE_DIR / "static", ]
