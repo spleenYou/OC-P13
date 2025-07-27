f5b09eabb0405a45bfef000cd3d5dc3d63d73989
Anthony YOU <spleen85@proton.me>
2025-07-27 23:00:51 +0200

    Change whitenoise storage

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -127,4 +127,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
